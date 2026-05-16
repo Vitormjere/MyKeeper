144cbdfb326cbca627c3eecdf791edc3e719b72f
Rodrigo Yuji <yuji.rodrigo@ufpr.br>
2026-05-16 16:42:31 -0300

    Função para encapsular e arrumar sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,39 +1,15 @@
-document.getElementById('inicioButtonLink').addEventListener('click', () => {
-    window.location.href = '/mykeeper/src/Views/home.php';
-});
+function addClick(id, url) {
+    const el = document.getElementById(id);
+    if (el) el.addEventListener('click', () => window.location.href = url);
+}
 
-document.getElementById('produtosButtonLink').addEventListener('click', () => {
-    window.location.href = '/mykeeper/src/Views/produto.php';
-});   
-
-document.getElementById('categoriasButtonLink').addEventListener('click', () => {
-    window.location.href = '/mykeeper/src/Views/categoria.php';
-});
-
-document.getElementById('logoffButtonLink').addEventListener('click', () => {
-    window.location.href = '/mykeeper/src/Controllers/logoff.php';
-});
-
-document.getElementById('perfilButtonLink').addEventListener('click', () => {
-    window.location.href = '/mykeeper/src/Views/perfil_usuario.php';
-});
-
-document.getElementById('ticketButtonLink').addEventListener('click', () => {
-    window.location.href = '/mykeeper/src/Views/ticket_usuario.php';
-});
-
-document.getElementById('receitasButtonLink').addEventListener('click', () => {
-    window.location.href = '/mykeeper/src/Views/receitas.php';
-})
-
-document.getElementById('estoquesButtonLink').addEventListener('click', () => {
-    window.location.href = '/mykeeper/src/Views/estoque.php';
-});
-
-document.getElementById('comprasButtonLink').addEventListener('click', () => {
-    window.location.href = '/mykeeper/src/Views/compras.php';
-});
-
-document.getElementById('adminHomeButtonLink').addEventListener('click', () => {
-    window.location.href = '/mykeeper/src/Views/admin_home.php';
-});
+addClick('inicioButtonLink',    '/mykeeper/src/Views/home.php');
+addClick('estoquesButtonLink',  '/mykeeper/src/Views/estoque.php');
+addClick('comprasButtonLink',   '/mykeeper/src/Views/compras.php');
+addClick('produtosButtonLink',  '/mykeeper/src/Views/produto.php');
+addClick('categoriasButtonLink','/mykeeper/src/Views/categoria.php');
+addClick('receitasButtonLink',  '/mykeeper/src/Views/receitas.php');
+addClick('perfilButtonLink',    '/mykeeper/src/Views/perfil_usuario.php');
+addClick('ticketButtonLink',    '/mykeeper/src/Views/ticket_usuario.php');
+addClick('logoffButtonLink',    '/mykeeper/src/Controllers/logoff.php');
+addClick('adminHomeButtonLink', '/mykeeper/src/Views/admin_home.php');
